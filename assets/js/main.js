ef7df677f397c1c11f54d46cd58b65a30eac242d
Bright199 <brightphiri94@gmail.com>
2022-05-18 18:29:02 +0300

    tour

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function () {
+ 
+})
+
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    autoplay:{
+
+    },
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
